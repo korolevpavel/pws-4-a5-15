070c1ad4c899df156dbe59279688b3df17cba137
Korolev <korolev@MacBook-Pro-Korolev.local>
2019-09-15 22:09:22 +0300

    Реализовано заполнение через циклы

--- js/ryaba.js
+++ js/ryaba.js
@@ -1,22 +1,39 @@
 const dataURL = "https://api.myjson.com/bins/jcmhn";
 
 function handleButton() {
-  // взять данные по dataUrl, вытащить их и передать в handleData
+
+  $.getJSON(
+    dataURL,
+    function (data) {
+      handleData(data);
+    }
+  )
 }
 
 function handleData(data) {
-  // кажется, какой-то из этих способов сработает
-  //const var1 = $("input[name=var1]")[0].value()
-  //const var1 = $("input[name=var1]").value()
-  //const var1 = $("input[name=var1]")[0].default()
 
-  // надо сделать так чтобы сообщения подменились на значения из формы
-  let text = "Здесь могла быть ваша реклама";
-	$("#result").text(text);
+  const formArray = $(".form-control");
+
+  // Разбираем массив в строку
+  let text = "";
+  for (let i = 0; i < data["text"].length; i++) {
+    text = text + data["text"][i] + " ";
+  }
+
+  // Заменяем значения
+  for (let i = 1; i <= formArray.length; i++) {
+    varReplace = "{var" + i + "}";
+    text = text.split(varReplace).join(formArray[i - 1].value)
+  }
+
+  text = text.replace("{speach}", formArray[formArray.length - 1].value);
+
+  $("#result").text(text);
+
 }
 
 function init() {
-	$("#button-fetch").click(handleButton);
+  $("#button-fetch").click(handleButton);
 }
 
 $(document).ready(init);
